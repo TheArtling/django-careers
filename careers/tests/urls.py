7d6d420e07292991b674c0290116cd8d66286ea5
Nicholas Ng <nzy.nicholas@gmail.com>
2016-10-26 11:18:26 +0800

    Fixed urls

--- careers/tests/urls.py
+++ careers/tests/urls.py
@@ -8,5 +8,4 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^careers/', include('careers.urls')),
-    url(r'^markdown/', include('django_markdown.urls')),
 ]
--- careers/urls.py
+++ careers/urls.py
@@ -1,8 +1,9 @@
 """URLs for the careers app."""
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.CareersView.as_view(), name='careers'),
+    url(r'^markdown/', include('django_markdown.urls')),
 ]
