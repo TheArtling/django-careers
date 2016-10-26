b8b74f3fb9daddcd79bc9fa1cc40c60f65dad9a9
Nicholas Ng <nzy.nicholas@gmail.com>
2016-10-26 11:29:35 +0800

    Removed django_markdown urls

--- careers/tests/urls.py
+++ careers/tests/urls.py
@@ -8,4 +8,5 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^careers/', include('careers.urls')),
+    url(r'^markdown/', include('django_markdown.urls')),
 ]
--- careers/urls.py
+++ careers/urls.py
@@ -1,9 +1,8 @@
 """URLs for the careers app."""
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.CareersView.as_view(), name='careers'),
-    url(r'^markdown/', include('django_markdown.urls')),
 ]
